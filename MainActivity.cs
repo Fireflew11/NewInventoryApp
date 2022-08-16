4a0a8ce27c03fd3750689ea5dabd2ede33a94b4e
Tom Braudo <tombraudo2808@gmail.com>
2022-08-16 19:14:14 +0300

    scrollview test run

--- MainActivity.cs
+++ MainActivity.cs
@@ -2,18 +2,41 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
 
 namespace NewInventoryApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
     {
+        Button btnSignIn;
+
+        public void OnClick(View v)
+        {
+
+            if(v == btnSignIn)
+            {
+                btnSignIn.SetTextColor(Android.Graphics.Color.Black);
+            }
+
+
+
+
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.ButtonXml);
+
+
+            btnSignIn = (Button)FindViewById(Resource.Id.buttonSignIn);
+            btnSignIn.SetOnClickListener(this);
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -21,5 +44,7 @@ namespace NewInventoryApp
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        
     }
 }
